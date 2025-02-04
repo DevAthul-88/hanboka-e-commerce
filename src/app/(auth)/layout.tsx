a66b3e1204e895d5d6d11d89b5fc105c0e8feaab
DevAthul-88 <devathulvinod@gmail.com>
2025-02-04 12:33:07 +0530

    test

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,10 @@
+"use client"
+
 import { useAuthenticatedBlitzContext } from "../blitz-server"
 
 export default async function AuthLayout({ children }: { children: React.ReactNode }) {
+  await useAuthenticatedBlitzContext({
+    redirectAuthenticatedTo: "/",
+  })
   return <>{children}</>
 }
